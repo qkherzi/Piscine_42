41314c507b2b856c3cf71ae06a169bdb8e0e2f92
qkherzi <78111978+qkherzi@users.noreply.github.com>
2021-01-29 19:41:33 +0100

    Add files via upload

---
+++ Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+cat /etc/passwd | grep -v "^#" | awk "NR%2==0" | cut -f 1 -d ":" | rev | sort -r | awk "NR >= $FT_LINE1 && NR <= $FT_LINE2" | tr "\n" "," | sed "s/,/, /g" | sed "s/, $/./" | tr -d '\n'
